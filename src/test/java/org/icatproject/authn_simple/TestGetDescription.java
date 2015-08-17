a3e06c1b5039fa08c4eedae6cae828e834f8e264
Steve Fisher <dr.s.m.fisher@gmail.com>
2015-08-17 11:14:36 +0100

    Move to authn.simple

---
+++ src/test/java/org/icatproject/authn_simple/TestGetDescription.java
@@ -0,0 +1,16 @@
+package org.icatproject.authn_simple;
+
+import static org.junit.Assert.assertEquals;
+
+import org.icatproject.authentication.Authenticator;
+import org.icatproject.authn_anon.ANON_Authenticator;
+import org.junit.Test;
+
+public class TestGetDescription {
+	@Test
+	public void test() throws Exception {
+		Authenticator a = new SIMPLE_Authenticator();
+		assertEquals("{\"keys\":[{\"name\":\"username\"},{\"name\":\"password\",\"hide\":true}]}", a.getDescription());
+
+	}
+}
