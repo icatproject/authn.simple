8392bb449c34779cf32a3e9802775009572590d0
Alex Kemp <alexander.kemp@stfc.ac.uk>
2024-08-29 12:07:10 +0100

    added ConfigProperty so can automatically pick up config from file

--- src/main/java/org/icatproject/authn_simple/SIMPLE_Authenticator.java
+++ src/main/java/org/icatproject/authn_simple/SIMPLE_Authenticator.java
@@ -4,10 +4,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.net.HttpURLConnection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import jakarta.annotation.PostConstruct;
-import jakarta.ejb.Stateless;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonReader;
@@ -21,161 +24,156 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.icatproject.authentication.AuthnException;
 import org.icatproject.authentication.PasswordChecker;
 import org.icatproject.utils.AddressChecker;
 import org.icatproject.utils.AddressCheckerException;
-import org.icatproject.utils.CheckedProperties;
-import org.icatproject.utils.CheckedProperties.CheckedPropertyException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
+
+import org.jboss.logging.Logger;
 
 /* Mapped name is to avoid name clashes */
-@Path("/")
-@Stateless
+@Path("/authn.simple")
+@ApplicationScoped
 public class SIMPLE_Authenticator {
 
-	private static final Logger logger = LoggerFactory.getLogger(SIMPLE_Authenticator.class);
-	private static final Marker fatal = MarkerFactory.getMarker("FATAL");
+    private static final Logger logger = Logger.getLogger(SIMPLE_Authenticator.class);
 
-	private Map<String, String> passwordtable;
-	private AddressChecker addressChecker;
-	private String mechanism;
+    private Map<String, String> passwordtable;
+    private AddressChecker addressChecker;
 
-	@PostConstruct
-	private void init() {
-		CheckedProperties props = new CheckedProperties();
-		try {
-			props.loadFromResource("run.properties");
+    @Inject
+    @ConfigProperty(name="user.list")
+    private List<String> users;
 
-			// Build the passwordtable out of user.list and
-			// user.<usern>.password
-			passwordtable = new HashMap<String, String>();
-			String[] users = props.getString("user.list").split("\\s+");
+    @Inject
+    @ConfigProperty(name="mechanism", defaultValue="simple")
+    private String mechanism;
 
-			String msg = "users configured [" + users.length + "]: ";
-			for (String user : users) {
-				passwordtable.put(user, props.getString("user." + user + ".password"));
-				msg = msg + user + " ";
-			}
-			logger.debug(msg);
+    @Inject
+    @ConfigProperty(name="ip")
+    private Optional<String> ipProperty;
 
-			if (props.has("ip")) {
-				String authips = props.getString("ip");
-				try {
-					addressChecker = new AddressChecker(authips);
-				} catch (Exception e) {
-					msg = "Problem creating AddressChecker with information from run.properties " + e.getMessage();
-					logger.error(fatal, msg);
-					throw new IllegalStateException(msg);
-				}
-			}
+    @Inject
+    @ConfigProperty(name = "quarkus.application.version")
+    String projectVersion;
 
-			// Note that the mechanism is optional
-			if (props.has("mechanism")) {
-				mechanism = props.getString("mechanism");
-			}
+    @PostConstruct
+    private void init() {
 
-		} catch (CheckedPropertyException e) {
-			logger.error(fatal, e.getMessage());
-			throw new IllegalStateException(e.getMessage());
-		}
+        passwordtable = new HashMap<String, String>();
+        // Populate the password table using the injected users list
+        for (String user : users) {
+            String password = ConfigProvider.getConfig().getValue("user." + user + ".password", String.class);
+            passwordtable.put(user, password);
+        }
 
-		logger.debug("Initialised SIMPLE_Authenticator");
-	}
+        String msg = "users configured [" + users.size() + "]: " + String.join(" ", users);
+        logger.debug(msg);
 
-	@GET
-	@Path("version")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getVersion() {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		JsonGenerator gen = Json.createGenerator(baos);
-		gen.writeStartObject().write("version", Constants.API_VERSION).writeEnd();
-		gen.close();
-		return baos.toString();
-	}
+        // Initialize the AddressChecker if the IP property is present
+        ipProperty.ifPresent(ip -> {
+            try {
+                addressChecker = new AddressChecker(ip);
+            } catch (Exception e) {
+                logger.error("Problem creating AddressChecker with IP: " + ip, e);
+                throw new IllegalStateException("Invalid IP configuration", e);
+            }
+        });
 
-	@POST
-	@Path("authenticate")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	@Produces(MediaType.APPLICATION_JSON)
-	public String authenticate(@FormParam("json") String jsonString) throws AuthnException {
+        logger.debug("Initialised SIMPLE_Authenticator");
+    }
 
-		ByteArrayInputStream s = new ByteArrayInputStream(jsonString.getBytes());
+    @GET
+    @Path("version")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getVersion() {
+        JsonObject versionJson = Json.createObjectBuilder()
+                .add("version", projectVersion)
+                .build();
+        return versionJson.toString();
+    }
 
-		String username = null;
-		String password = null;
-		String ip = null;
-		try (JsonReader r = Json.createReader(s)) {
-			JsonObject o = r.readObject();
-			for (JsonValue c : o.getJsonArray("credentials")) {
-				JsonObject credential = (JsonObject) c;
-				if (credential.containsKey("username")) {
-					username = credential.getString("username");
-				} else if (credential.containsKey("password")) {
-					password = credential.getString("password");
-				}
-			}
-			if (o.containsKey("ip")) {
-				ip = o.getString("ip");
-			}
+    @POST
+    @Path("authenticate")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String authenticate(@FormParam("json") String jsonString) throws AuthnException {
 
-		}
+        ByteArrayInputStream s = new ByteArrayInputStream(jsonString.getBytes());
 
-		logger.debug("Login request by: " + username);
+        String username = null;
+        String password = null;
+        String ip = null;
+        try (JsonReader r = Json.createReader(s)) {
+            JsonObject o = r.readObject();
+            for (JsonValue c : o.getJsonArray("credentials")) {
+                JsonObject credential = (JsonObject) c;
+                if (credential.containsKey("username")) {
+                    username = credential.getString("username");
+                } else if (credential.containsKey("password")) {
+                    password = credential.getString("password");
+                }
+            }
+            if (o.containsKey("ip")) {
+                ip = o.getString("ip");
+            }
 
-		if (username == null || username.isEmpty()) {
-			throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN, "username cannot be null or empty.");
-		}
+        }
 
-		if (password == null || password.isEmpty()) {
-			throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN, "password cannot be null or empty.");
-		}
+        logger.debug("Login request by: " + username);
 
-		if (addressChecker != null) {
-			try {
-				if (!addressChecker.check(ip)) {
-					throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN,
-							"authn.simple does not allow log in from your IP address " + ip);
-				}
-			} catch (AddressCheckerException e) {
-				throw new AuthnException(HttpURLConnection.HTTP_INTERNAL_ERROR, e.getClass() + " " + e.getMessage());
-			}
-		}
+        if (username == null || username.isEmpty()) {
+            throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN, "username cannot be null or empty.");
+        }
 
-		String encodedPassword = passwordtable.get(username);
-		if (!PasswordChecker.verify(password, encodedPassword)) {
-			throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN, "The username and password do not match ");
-		}
+        if (password == null || password.isEmpty()) {
+            throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN, "password cannot be null or empty.");
+        }
 
-		logger.info(username + " logged in succesfully" + (mechanism != null ? " by " + mechanism : ""));
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try (JsonGenerator gen = Json.createGenerator(baos)) {
-			gen.writeStartObject().write("username", username);
-			if (mechanism != null) {
-				gen.write("mechanism", mechanism);
-			}
-			gen.writeEnd();
-		}
-		return baos.toString();
+        if (addressChecker != null) {
+            try {
+                if (!addressChecker.check(ip)) {
+                    throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN,
+                            "authn.simple does not allow log in from your IP address " + ip);
+                }
+            } catch (AddressCheckerException e) {
+                throw new AuthnException(HttpURLConnection.HTTP_INTERNAL_ERROR, e.getClass() + " " + e.getMessage());
+            }
+        }
 
-	}
+        String encodedPassword = passwordtable.get(username);
+        if (!PasswordChecker.verify(password, encodedPassword)) {
+            throw new AuthnException(HttpURLConnection.HTTP_FORBIDDEN, "The username and password do not match ");
+        }
 
-	@GET
-	@Path("description")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getDescription() {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try (JsonGenerator gen = Json.createGenerator(baos)) {
-			gen.writeStartObject().writeStartArray("keys");
-			gen.writeStartObject().write("name", "username").writeEnd();
-			gen.writeStartObject().write("name", "password").write("hide", true).writeEnd();
-			gen.writeEnd().writeEnd();
-		}
-		return baos.toString();
-	}
+        logger.info(username + " logged in succesfully" + (mechanism != null ? " by " + mechanism : ""));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (JsonGenerator gen = Json.createGenerator(baos)) {
+            gen.writeStartObject().write("username", username);
+            if (mechanism != null) {
+                gen.write("mechanism", mechanism);
+            }
+            gen.writeEnd();
+        }
+        return baos.toString();
+
+    }
+
+    @GET
+    @Path("description")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getDescription() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (JsonGenerator gen = Json.createGenerator(baos)) {
+            gen.writeStartObject().writeStartArray("keys");
+            gen.writeStartObject().write("name", "username").writeEnd();
+            gen.writeStartObject().write("name", "password").write("hide", true).writeEnd();
+            gen.writeEnd().writeEnd();
+        }
+        return baos.toString();
+    }
 
 }
